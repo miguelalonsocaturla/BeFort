df539976abfc37ae3f13f42c4f5a667fb4926626
miguelalonsocaturla <mikialonsoc@gmail.com>
2024-05-21 22:43:59 +0200

    Seccion login y administrador

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,6 @@ dependencies {
 
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
-    implementation ("com.google.firebase:firebase-database:20.0.5")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
@@ -49,6 +48,7 @@ dependencies {
     implementation("com.google.android.gms:play-services-maps:18.2.0")
     implementation("com.google.firebase:firebase-firestore:25.0.0")
     implementation("com.google.android.gms:play-services-location:21.2.0")
+    implementation("androidx.preference:preference:1.2.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/befort/LoginFragment.java
+++ app/src/main/java/com/example/befort/LoginFragment.java
@@ -1,5 +1,9 @@
 package com.example.befort;
 
+import static android.content.Context.MODE_PRIVATE;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
@@ -11,92 +15,110 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class LoginFragment extends Fragment {
 
     private EditText editTextEmail, editTextPassword;
-    private DatabaseReference usersRef;
+    private FirebaseFirestore db;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflar el diseño del fragmento
         View view = inflater.inflate(R.layout.fragment_login, container, false);
 
-        // Obtener referencias a los campos de entrada dentro de la vista inflada
-        editTextEmail = view.findViewById(R.id.editTextEmail);
-        editTextPassword = view.findViewById(R.id.editTextPassword);
-
-        // Inicializar la referencia a la base de datos de Firebase
-        usersRef = FirebaseDatabase.getInstance().getReference("usuarios");
-
-        // Obtener referencia al botón de inicio de sesión dentro de la vista inflada
-        Button buttonLogin = view.findViewById(R.id.buttonLogin);
+        // Inicializar vistas y Firebase
+        initViews(view);
+        initFirebase();
 
         // Configurar el evento de clic del botón de inicio de sesión
-        buttonLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Obtener valores de los campos de entrada
-                String email = editTextEmail.getText().toString().trim();
-                String password = editTextPassword.getText().toString().trim();
+        configureLoginButton(view);
 
-                // Validar campos vacíos
-                if (TextUtils.isEmpty(email)) {
-                    editTextEmail.setError("Por favor ingresa tu correo electrónico");
-                    return;
-                }
-
-                if (TextUtils.isEmpty(password)) {
-                    editTextPassword.setError("Por favor ingresa tu contraseña");
-                    return;
-                }
-
-                // Comprobar las credenciales del usuario en Firebase
-                checkUserCredentials(email, password);
-            }
-        });
-
-        // Devolver la vista inflada con las referencias configuradas
         return view;
     }
 
-    private void checkUserCredentials(final String email, final String password) {
-        usersRef.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                boolean userExists = false;
-                String userType = null;
+    private void initViews(View view) {
+        editTextEmail = view.findViewById(R.id.editTextEmail);
+        editTextPassword = view.findViewById(R.id.editTextPassword);
+    }
 
-                for (DataSnapshot userSnapshot : dataSnapshot.getChildren()) {
-                    String dbEmail = userSnapshot.child("email").getValue(String.class);
-                    String dbPassword = userSnapshot.child("password").getValue(String.class);
-                    if (dbEmail != null && dbEmail.equals(email) && dbPassword != null && dbPassword.equals(password)) {
-                        userExists = true;
-                        userType = userSnapshot.child("tipo").getValue(String.class);
-                        break;
-                    }
-                }
+    private void initFirebase() {
+        db = FirebaseFirestore.getInstance();
+    }
 
-                if (userExists) {
-                    // Inicio de sesión exitoso
-                    Toast.makeText(getActivity(), "Inicio de sesión exitoso, Tipo: " + userType, Toast.LENGTH_SHORT).show();
-                    // Aquí puedes manejar la variable userType como desees
-                } else {
-                    // Credenciales incorrectas, mostrar mensaje de error
-                    Toast.makeText(getActivity(), "Credenciales incorrectas", Toast.LENGTH_SHORT).show();
-                }
-            }
+    private void configureLoginButton(View view) {
+        Button buttonLogin = view.findViewById(R.id.buttonLogin);
+        buttonLogin.setOnClickListener(v -> {
+            String email = editTextEmail.getText().toString().trim();
+            String password = editTextPassword.getText().toString().trim();
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                // Manejar errores de la base de datos
-                Toast.makeText(getActivity(), "Error al comprobar las credenciales", Toast.LENGTH_SHORT).show();
+            if (validateInputs(email, password)) {
+                checkUserCredentials(email, password);
             }
         });
+    }
+
+    private boolean validateInputs(String email, String password) {
+        if (TextUtils.isEmpty(email)) {
+            editTextEmail.setError("Por favor ingresa tu correo electrónico");
+            return false;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            editTextPassword.setError("Por favor ingresa tu contraseña");
+            return false;
+        }
+
+        return true;
+    }
+
+    private void checkUserCredentials(final String email, final String password) {
+        db.collection("usuarios")
+                .whereEqualTo("usuario", email)
+                .whereEqualTo("contraseña", password)
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        QuerySnapshot querySnapshot = task.getResult();
+                        if (querySnapshot != null && !querySnapshot.isEmpty()) {
+                            DocumentSnapshot document = querySnapshot.getDocuments().get(0);
+                            Boolean userType = (Boolean) document.get("tipo");
+
+                            handleLoginSuccess(userType);
+                        } else {
+                            showToast("Credenciales incorrectas");
+                        }
+                    } else {
+                        showToast("Error al comprobar las credenciales");
+                    }
+                });
+    }
+
+    private void handleLoginSuccess(Boolean userType) {
+        saveUserType(userType);
+        showToast("Inicio de sesión exitoso");
+        // Aquí puedes manejar la variable userType como desees
+
+        // Si el tipo de usuario es true, inicia AdminActivity
+        if (userType != null && userType) {
+            Intent intent = new Intent(getActivity(), NewParqueFragment.class);
+            startActivity(intent);
+        } else {
+            // Maneja la lógica para otros tipos de usuarios aquí
+        }
+    }
+
+    private void saveUserType(Boolean userType) {
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences("UserPreferences", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean("admin", userType);
+        editor.apply();
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(getActivity(), message, Toast.LENGTH_SHORT).show();
     }
 }
